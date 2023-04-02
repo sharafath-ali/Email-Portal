ed3f85f68364523c7ab020c3cebce70a3e96cb8d
sharafath ali <sharafathaliofficial@gmail.com>
2023-04-02 17:58:44 +0530

    j

--- src/Mail.js
+++ src/Mail.js
@@ -1,65 +1,80 @@
-import { IconButton } from '@material-ui/core'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import React from 'react'
-import DeleteIcon from '@mui/icons-material/Delete';
-import MoveToInboxIcon from '@mui/icons-material/MoveToInbox';
-import EmailIcon from '@mui/icons-material/Email';
-import ErrorIcon from '@mui/icons-material/Error';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import WatchLaterIcon from '@mui/icons-material/WatchLater';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import LabelImportantIcon from '@mui/icons-material/LabelImportant';
-import PrintIcon from '@mui/icons-material/Print';
-import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import './Mail.css'
+import { IconButton } from "@material-ui/core";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import MoveToInboxIcon from "@mui/icons-material/MoveToInbox";
+import EmailIcon from "@mui/icons-material/Email";
+import ErrorIcon from "@mui/icons-material/Error";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import LabelImportantIcon from "@mui/icons-material/LabelImportant";
+import PrintIcon from "@mui/icons-material/Print";
+import UnfoldMoreIcon from "@mui/icons-material/UnfoldMore";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import "./Mail.css";
 import { useNavigate } from "react-router-dom";
 function Mail() {
-  const history=useNavigate();
+  const history = useNavigate();
   return (
-    <div className='Mail' >
-      <div className='Mail_tool'>
-        <div className='Mail_tool_left'>
-          <IconButton  onClick={()=>{history('/')}}>
-              <ArrowBackIcon/>
+    <div className="Mail">
+      <div className="Mail_tool">
+        <div className="Mail_tool_left">
+          <IconButton
+            onClick={() => {
+              history("/");
+            }}
+          >
+            <ArrowBackIcon />
           </IconButton>
           <IconButton>
-             <MoveToInboxIcon/>
+            <MoveToInboxIcon />
           </IconButton>
           <IconButton>
-            <ErrorIcon/>
+            <ErrorIcon />
           </IconButton>
           <IconButton>
-            <DeleteIcon/>
+            <DeleteIcon />
           </IconButton>
           <IconButton>
-            <EmailIcon/>
-          </IconButton>
-          <IconButton><WatchLaterIcon style={{color:'gray'}}/></IconButton>
-          <IconButton>
-            < CheckCircleIcon />
+            <EmailIcon />
           </IconButton>
           <IconButton>
-          <LabelImportantIcon style={{color:'gray'}}/>
+            <WatchLaterIcon style={{ color: "gray" }} />
           </IconButton>
           <IconButton>
-          <MoreVertIcon style={{color:'gray'}}/>
+            <CheckCircleIcon />
+          </IconButton>
+          <IconButton>
+            <LabelImportantIcon style={{ color: "gray" }} />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon style={{ color: "gray" }} />
           </IconButton>
         </div>
-        <div className='Mail_tool_right'>
+        <div className="Mail_tool_right">
           <IconButton>
-            <UnfoldMoreIcon/>
+            <UnfoldMoreIcon />
           </IconButton>
           <IconButton>
-            <PrintIcon/>
+            <PrintIcon />
           </IconButton>
           <IconButton>
-            <ExitToAppIcon/>
+            <ExitToAppIcon />
           </IconButton>
         </div>
       </div>
+      <div className="mail_body">
+        <div className="mail_body_header">
+          <h2>Subject</h2>
+          <LabelImportantIcon className="mail_important" />
+          <p>Title</p>
+          <p>10pm</p>
+        </div>
+        <div className="mail_message">this is a message</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Mail
+export default Mail;
